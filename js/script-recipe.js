80ded1d3f4c69af8f56b1e8f4ce9105b27930e81
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-07-10 19:39:53 -0400

    Etapa 8: Se asignan las funciones click a las pestañas y se les agrega o quita la clase make según corresponda.

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,3 +1,17 @@
+//Etapa 7:
 $(document).ready( function(){
 	console.log("HTML listo.");
+
+//Etapa 8:
+$(".js-show-recipe").click(function(event) {
+		console.log("js-show-recipe");
+		$(".page.recipe").removeClass('make');	
+	});
+
+	$(".js-show-make").click(function(event) {
+		console.log("js-show-make");
+		$(".page.recipe").addClass('make');
+	});
+
+
 });
