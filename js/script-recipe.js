d4dbfd5e8201a84713c8275e9100bc733457ad75
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-07-10 19:43:32 -0400

    Etapa 9: Se les agrega o quita la clase active a los botones recipe y make it según corresponda, es decir cuando se hace click en uno o en otro.

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,15 +2,19 @@
 $(document).ready( function(){
 	console.log("HTML listo.");
 
-//Etapa 8:
+//Etapa 8 y 9:
 $(".js-show-recipe").click(function(event) {
 		console.log("js-show-recipe");
 		$(".page.recipe").removeClass('make');	
+		$(this).addClass('active');
+		$(".js-show-make").removeClass('active');
 	});
 
 	$(".js-show-make").click(function(event) {
 		console.log("js-show-make");
 		$(".page.recipe").addClass('make');
+		$(this).addClass('active');
+		$(".js-show-recipe").removeClass('active');
 	});
 
 
